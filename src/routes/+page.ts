5eacdcf476bed029336916a249272375b6140254
Dead End <dead.end@inbox.ru>
2025-01-18 12:44:11 +0100

    added example

---
+++ src/routes/+page.ts
@@ -0,0 +1,14 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ params }) => {
+	const url =
+		'https://raw.githubusercontent.com/dead-end/cards-russian/master/data/book/chap-15/chunk-1.json';
+	const res = await fetch(url);
+	const json = await res.json();
+	console.log('json', json);
+	return {
+		post: {
+			status: 'ok'
+		}
+	};
+};
