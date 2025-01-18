b6f9b897735c515556c22d445d406af02b813179
Dead End <dead.end@inbox.ru>
2025-01-18 13:23:58 +0100

    added spa example

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,3 @@
 // @sveltejs/adapter-static: all routes must be fully prerenderable, but found the following routes that are dynamic:
-export const prerender = true;
+export const prerender = false;
+export const ssr = false;
