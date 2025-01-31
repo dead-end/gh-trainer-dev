5aba46f2c7dcfed50ad72225f7ad75522118e96d
Dead End <dead.end@inbox.ru>
2025-02-01 00:08:13 +0100

    added area

---
+++ src/lib/components/form/Area.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Wrapper from './Wrapper.svelte';
+
+	type Props = {
+		id: string;
+		label: string;
+		value: string;
+		error: string;
+	};
+
+	let { id, label, value = $bindable(), error }: Props = $props();
+</script>
+
+<Wrapper {id} {label} {error}>
+	<textarea class="input" {id} bind:value></textarea>
+</Wrapper>
