7e45c71f6893af4b878020a698ce02344c5e8c46
Dead End <dead.end@inbox.ru>
2025-02-01 00:10:38 +0100

    books functions

---
+++ src/lib/models/book.ts
@@ -0,0 +1,10 @@
+import { githubGetUrl, githubReadContent } from '$lib/github';
+import { githubConfig } from '$lib/github/config.svelte';
+import type { TBook } from '$lib/types';
+
+export const bookListing = async () => {
+	const url = githubGetUrl(githubConfig.user, githubConfig.repo, 'books/listing.books.json');
+	const data = await githubReadContent(url, githubConfig.token);
+	const books: TBook[] = JSON.parse(data.content);
+	return books;
+};
