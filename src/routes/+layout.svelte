007cbc73a2cb41e199a9dd6853f86412cd4eb8e1
Dead End <dead.end@inbox.ru>
2025-02-01 00:09:10 +0100

    added navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import Navigation from '$lib/components/layout/Navigation.svelte';
 	import '../app.css';
 	import Footer from '../lib/components/layout/Footer.svelte';
 	let { children } = $props();
 </script>
 
 <main class="m-auto mt-4 max-w-screen-lg px-4 text-gray-600 lg:px-0">
+	<Navigation />
 	{@render children()}
 	<Footer />
 </main>
